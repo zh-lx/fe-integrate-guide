8080e0b68b65f3ee3c19ed19dae8066fc3abae69
zh-lx <18366276315@163.com>
2021-12-08 09:37:57 +0800

    feat: 初始化

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
+};
